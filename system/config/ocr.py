c81001cea059b70027752a10da52c5b823c9930b
Monster013 <1monsteruniverse@gmail.com>
2024-10-11 08:06:46 +0530

    Update ocr.py

--- system/config/ocr.py
+++ system/config/ocr.py
@@ -577,7 +577,7 @@ class PortForward:
           clear_output()
           loadingAn(name="lds")
           textAn("Starting Argo Tunnel ...", ty="twg")
-        data = dict(url="https://"+ArgoTunnel(port, proto, closePort(self.config[1])).keep_alive())
+        data = dict(url="https://"+ArgoTunnel(port, proto, closePort).keep_alive())
         if displayB:
           displayUrl(data, btc)
         return data
